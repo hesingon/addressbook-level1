728e488586e63ac19b99da8285d74f8cf36f1a15
He Xing Darius <Hexane.gmf@gmail.com>
2017-01-25 21:27:26 +0800

    Disrupt SLAP in main function

--- src/seedu/addressbook/AddressBook.java
+++ src/seedu/addressbook/AddressBook.java
@@ -215,13 +215,24 @@ public class AddressBook {
 
 	public static void main(String[] args) {
 		showWelcomeMessage();
-		processProgramArgs(args);
+
+		if (args.length >= 2) {
+			showToUser(MESSAGE_INVALID_PROGRAM_ARGS);
+			exitProgram();
+		}
+
+		if (args.length == 1) {
+			setupGivenFileForStorage(args[0]);
+		}
+
+		if (args.length == 0) {
+			setupDefaultFileForStorage();
+		}
+
 		loadDataFromStorage();
 		while (true) {
-			String userCommand = getUserInput();
-			echoUserCommand(userCommand);
-			String feedback = executeCommand(userCommand);
-			showResultToUser(feedback);
+			String userCommand = getUserInput(); echoUserCommand(userCommand);
+			String feedback = executeCommand(userCommand); showResultToUser(feedback);
 		}
 	}
 
@@ -264,20 +275,6 @@ public class AddressBook {
 	 * @param args
 	 *            full program arguments passed to application main method
 	 */
-	private static void processProgramArgs(String[] args) {
-		if (args.length >= 2) {
-			showToUser(MESSAGE_INVALID_PROGRAM_ARGS);
-			exitProgram();
-		}
-
-		if (args.length == 1) {
-			setupGivenFileForStorage(args[0]);
-		}
-
-		if (args.length == 0) {
-			setupDefaultFileForStorage();
-		}
-	}
 
 	/**
 	 * Sets up the storage file based on the supplied file path. Creates the
